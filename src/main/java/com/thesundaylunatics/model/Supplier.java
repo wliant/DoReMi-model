f14b2abfb0ddd0b1898a3a9e01f71a75f4db7441
tangmeng <tangmeng1993@gmail.com>
2019-03-02 13:06:20 +0800

    add on supplier model

---
+++ src/main/java/com/thesundaylunatics/model/Supplier.java
@@ -0,0 +1,44 @@
+package com.thesundaylunatics.model;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="tb_supplier")
+public class Supplier {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+	@OneToOne
+	@JoinColumn(name = "supplier_id", referencedColumnName = "id")
+	private String supplierId;
+
+    private String supplierName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier [id=" + id + "]";
+    }
+}
