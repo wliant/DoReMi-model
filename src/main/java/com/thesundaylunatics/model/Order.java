cc1c13825f50cd10a59d2f93f8d38cb542430269
tangmeng <tangmeng1993@gmail.com>
2019-03-06 16:26:00 +0800

    Update Order.java
    -change fetch type to eager

--- src/main/java/com/thesundaylunatics/model/Order.java
+++ src/main/java/com/thesundaylunatics/model/Order.java
@@ -4,16 +4,7 @@ import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Date;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="tb_order")
@@ -76,7 +67,8 @@ public class Order {
 	}
 
 	@OneToMany(
-            cascade = CascadeType.ALL,
+			fetch = FetchType.EAGER,
+			cascade = CascadeType.ALL,
             mappedBy = "order",
             orphanRemoval = true
         )
